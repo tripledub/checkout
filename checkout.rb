2c0492ee87e943f9e96bce34c8b04f921d35d1db
Stewart Campbell <stewartcuk@gmail.com>
2020-10-02 15:45:23 +0200

    first pass at checkout, all products no rules

--- checkout.rb
+++ checkout.rb
@@ -1,5 +1,44 @@
+Product = Struct.new(:sku, :price, :title)
+
+PRODUCTS = [
+  Product.new('001', 9.25, 'Lavender heart'),
+  Product.new('002', 45, 'Personalised cufflinks'),
+  Product.new('003', 19.95, 'Kids T-shirt')
+].freeze
+
 class Checkout
+  DISCOUNT_AT = 60
+  DISCOUNT_AMOUNT = 10
+
+  def scan(sku:)
+    items[sku] ||= 0
+    items[sku] += 1
+  end
+
   def total
-    0
+    sumtotal = items.inject(0) do |subtotal, (sku, qty)|
+      subtotal + price_for(sku: sku, quantity: qty)
+    end
+
+    sumtotal >= DISCOUNT_AT ? discounted_total(sumtotal: sumtotal) : sumtotal
+  end
+
+  private
+
+  def discounted_total(sumtotal:)
+    sumtotal - (sumtotal * 0.1)
+  end
+
+  def items
+    @items ||= {}
+  end
+
+  def price_for(sku:, quantity:)
+    line_item = product_for(sku: sku)
+    line_item.price * quantity
+  end
+
+  def product_for(sku:)
+    PRODUCTS.find { |product| product.sku == sku }
   end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -4,9 +4,31 @@ RSpec.describe Checkout do
   subject { described_class.new }
 
   describe ':total' do
+
+    # Product code  | Name                   | Price
+    # ----------------------------------------------------------
+    # 001           | Lavender heart         | £9.25
+    # 002           | Personalised cufflinks | £45.00
+    # 003           | Kids T-shirt           | £19.95
+    #
+    # adding as comments here for now as not using factories
+    # for products so tests might not be so clear
+
     context 'when there are zero items in checkout' do
       it 'returns 0' do
         expect(subject.total).to eq(0)
+      end
+    end
+
+    context 'when there is one of each product' do
+      before do
+        subject.scan(sku: '001')
+        subject.scan(sku: '002')
+        subject.scan(sku: '003')
+      end
+
+      it 'returns the sum of each product' do
+        expect(subject.total).to eq(66.78)
       end
     end
   end
