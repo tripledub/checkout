31ea8083034e61932298888d315bcd055bd5f6bd
Stewart Campbell <stewartcuk@gmail.com>
2020-10-02 18:16:00 +0200

    more spec refactors

--- spec/discount_rule_spec.rb
+++ spec/discount_rule_spec.rb
@@ -3,15 +3,20 @@ require_relative '../discount_rule'
 RSpec.describe DiscountRule do
   let(:original_price) { 15 }
   let(:discount_price) { 12 }
+  let(:minimum_amount) { 4 }
+
   let(:rule) do
-    described_class.new(discount_price: discount_price, minimum_amount: 4, sku: '001')
+    described_class.new(
+      discount_price: discount_price,
+      minimum_amount: minimum_amount,
+      sku: '001'
+    )
   end
 
   describe ':price_for' do
-    let(:quantity) { 5 }
-    let(:original_price) { 15 }
-
     context 'when minimum amount met' do
+      let(:quantity) { minimum_amount }
+
       it 'applies discount' do
         expect(
           rule.price_for(quantity: quantity, original_price: original_price)
@@ -20,7 +25,7 @@ RSpec.describe DiscountRule do
     end
 
     context 'when minimum amount not met' do
-      let(:quantity) { 1 }
+      let(:quantity) { minimum_amount - 1 }
 
       it 'applies no discount' do
         expect(
